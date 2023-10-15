0691e71e80817bb81f965d7bca6f26030721b78e
arkades <arkades@gmail.com>
2023-10-15 14:50:24 +0200

    Apka dzień 4 if else

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,3 +1,20 @@
-﻿Console.WriteLine(@"This is one line followed by a carriage return
-this is the second line of the string
-and this is the third line\");
+﻿var name = "Arek";
+char gender = 'm';
+var age = 17;
+
+if (age < 30 && gender == 'w')
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" &&age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Zwierzę jakieś");
+}
